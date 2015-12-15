4817465a22e97239b33e4d52123a77bbd01a1d5e
Avinash Jha <avinash.jha@msystechnologies.com>
2015-12-15 17:15:27 +0530

    Manage Certificates And Requests at puppet server. Reorganized the code for better readability

--- src/main/resources/project/procedures/RunCommand/driver.pl
+++ src/main/resources/project/procedures/RunCommand/driver.pl
@@ -14,6 +14,7 @@
 #  limitations under the License.
 #
 package RunCommandDriver;
+
 # -------------------------------------------------------------------------
 # Includes
 # -------------------------------------------------------------------------
@@ -26,13 +27,14 @@ use ElectricCommander;
 use ElectricCommander::PropMod qw(/myProject/libs);
 use PuppetHelper;
 
-$|=1;
+$| = 1;
 
 # -------------------------------------------------------------------------
 # Main functions
 # -------------------------------------------------------------------------
- 
+
 ###########################################################################
+
 =head2 main
  
   Title    : main
@@ -41,44 +43,50 @@ $|=1;
   Returns  : none
   Args     : named arguments: none
 =cut
+
 ###########################################################################
 
 sub main {
     my $ec = ElectricCommander->new();
     $ec->abortOnError(0);
-    
+
     # -------------------------------------------------------------------------
     # Parameters
     # -------------------------------------------------------------------------
-    $::g_puppet_path = ($ec->getProperty( "puppet_path" ))->findvalue('//value')->string_value;
-    $::g_puppet_command = ($ec->getProperty( "puppet_command" ))->findvalue('//value')->string_value;
+    $::g_puppet_path =
+      ( $ec->getProperty("puppet_path") )->findvalue('//value')->string_value;
+    $::g_puppet_command =
+      ( $ec->getProperty("puppet_command") )->findvalue('//value')
+      ->string_value;
 
-    
     my @cmd;
     my %props;
-    
+
     #Prints procedure and parameters information
     print "EC-Puppet: ";
     print "Run Command procedure \n\n";
-    
-	#Parameters are checked to see which should be included
-    if($::g_puppet_path && $::g_puppet_path ne '' && $::g_puppet_command && $::g_puppet_command ne '')
+
+    #Parameters are checked to see which should be included
+    if (   $::g_puppet_path
+        && $::g_puppet_path ne ''
+        && $::g_puppet_command
+        && $::g_puppet_command ne '' )
     {
         #Variable that stores the command to be executed
-		$::g_command = $::g_puppet_path . " " .$::g_puppet_command;
-	
-		print "Command to be executed: \n$::g_command \n\n";
-        
-		#Executes the command into puppet
-		system("$::g_command");
+        $::g_command = $::g_puppet_path . " " . $::g_puppet_command;
+
+        print "Command to be executed: \n$::g_command \n\n";
+
+        #Executes the command into puppet
+        system("$::g_command");
 
         # To get exit code of process shift right by 8
         my $exitCode = $? >> 8;
 
         # Set outcome
-        setOutcomeFromExitCode($ec, $exitCode);
+        setOutcomeFromExitCode( $ec, $exitCode );
 
     }
 }
-  
+
 main();
